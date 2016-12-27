65f7c77d41412fde5138504dd4e4007b748465a5
ronghui <antony_yeah@126.com>
2016-12-27 10:38:13 -0500

    A-0 refatoring

--- app/components/productNav/index.js
+++ app/components/productNav/index.js
@@ -1,72 +1,33 @@
+'use strict';
+
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  Dimensions,
-  TouchableOpacity
-} from 'react-native';
-import Dashboard from '../dashboard';
-
-export default class ProductNav extends Component {
-  constructor(props) {
-     super(props);
-     this._navigate = this._navigate.bind(this);
-   }
-
-   _navigate() {
-    this.props.navigator.push({
-      component: Dashboard
-    })
-   }
-
-   _toBeDetermine() {
-    Alert.alert(
-      'To Be Determine',
-      'Comming up soon ...'
-    ) 
-   }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity activeOpacity={.5} onPress={ this._navigate }>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>ECM</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity activeOpacity={.5} onPress={ this._toBeDetermine }>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>...</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+import { StyleSheet, NavigatorIOS } from 'react-native';
+import ProductNav from './productNav';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  button: {
-    height: 80,
-    width: 80,
-    backgroundColor: "#6ec4e9",
-    paddingVertical: 20,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    marginLeft: 15,
-    marginRight: 15
-  },
-  buttonText: {
-    color: "#FFF",
-    fontSize: 18,
-  }
+    container: {
+        flex: 1,
+        backgroundColor : '#6ec4e9'
+    }
 });
+
+class ProductNavigateScreen extends Component {
+    constructor(props) {
+     super(props);
+   }
+    render() {
+        return (
+            <NavigatorIOS
+                barTintColor='#6ec4e9'
+                titleTextColor='#fff'
+                tintColor='#fff'
+                style={styles.container}
+                initialRoute={{
+                    title: 'Product Navigation',
+                    component: ProductNav
+                }}/>
+        );
+    }
+}
+
+export default ProductNavigateScreen;
